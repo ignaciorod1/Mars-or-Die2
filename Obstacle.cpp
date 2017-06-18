c6f8876da19f0cd449073e1a30ff261d2184bc44
Ignacio <ignacio_rnp@aol.com>
2017-06-18 02:41:29 +0200

    Update Obstacle.cpp

--- Obstacle.cpp
+++ Obstacle.cpp
@@ -10,9 +10,9 @@ Obstacle::~Obstacle(void)
 {
 }
 
-void Obstacle::SetPos(float ix, float iy) {
-	position.x = ix;
-	position.y = iy;
+void Obstacle::SetPos(float ix, float iy) {  // también se puede inicializar con la sobrecarga del constructor de vector
+	position.x = ix;		     // Vector2(float x, float y) y en la definición se pondría SetPos(Vector2).
+	position.y = iy;		     // En esta situación son más cómodos dos floats, though
 }
 
 void Obstacle::SetAcc(float ix, float iy) {
